0aa066ca893d3f4e9de2cb53402fad821fb49741
isonetcorp@gmail.com <isonetcorp@gmail.com>
2025-01-14 18:00:13 -0400

    Commit Inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Home from './screens/Home.jsx';
+import Counter from "./Components/Counter/Counter.jsx";
+
+function App() {
+  return (
+    <>
+        <Home />
+        <Counter />
+    </>
+  )
+}
+
+export default App;
